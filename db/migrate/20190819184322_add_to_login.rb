5ff81b8e524b41aac1e51441b1bc8ab53aa0cf05
George Ruiz <georuiz817@gmail.com>
2019-08-19 18:49:39 +0000

    new migration for pw digest

---
+++ db/migrate/20190819184322_add_to_login.rb
@@ -0,0 +1,5 @@
+class AddToLogin < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190819162527) do
+ActiveRecord::Schema.define(version: 20190819184322) do
 
   create_table "cafes", force: :cascade do |t|
     t.string  "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190819162527) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
+    t.string "password_digest"
   end
 
 end
