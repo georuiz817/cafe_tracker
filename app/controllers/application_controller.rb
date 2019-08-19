30485a2d4558d064f3a54d43bd1dd10245c95323
George Ruiz <georuiz817@gmail.com>
2019-08-19 17:16:45 +0000

    login/sign up complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,42 @@ class ApplicationController < Sinatra::Base
  	get '/' do
     erb :home
   end
+
+#Must have user account requirement start
+  get '/signup' do
+        erb :'/users/signup'
+    end
+
+    post '/signup' do
+        user = User.create(params)
+        if user.save
+            session[:user_id] = user.id
+            redirect '/' 
+        else
+           erb :'users/signup'
+      end
+    end
+    #
+    get '/login' do
+        erb :'/users/login'
+    end
+
+   post '/login' do
+        user = User.find_by(email: params[:email])
+        
+        if user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect '/' 
+        else
+           erb :'users/signup'
+        end
+    end
+    #
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
+#must have user account requirement end
 end
+
