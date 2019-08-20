cbe02cf92c3fa8d16d33e16ee566d5da123a34da
George Ruiz <georuiz817@gmail.com>
2019-08-20 04:40:32 +0000

    beautified html

--- app/controllers/cafe_controller.rb
+++ app/controllers/cafe_controller.rb
@@ -4,14 +4,39 @@ class CafeController < ApplicationController
         erb :'cafes/index'
     end 
 
+    post '/cafes' do
+     @cafe = current_user.cafes.create(name: params["name"],location: params["location"],wifi_avaliable: params["wifi_avaliable"])
+     redirect '/cafes'
+    end 
+
     get '/cafes/new' do
         erb :'cafes/new'
     end
 
-    post '/cafes' do
-     @cafe = current_user.cafes.create(name: params["name"],location: params["location"],wifi_avaliable: params["wifi_avaliable"])
+    get '/cafes/:id' do
+        @cafe = Cafe.find_by_id(params[:id])
+        erb :'cafes/show'
     end 
 
+    get '/cafes/:id/edit' do 
+        @cafe = Cafe.find_by_id(params[:id])
+        erb :'cafes/edit'
+    end 
+
+    patch '/cafes/:id' do 
+        @cafe = Cafe.find_by_id(params[:id])
+        @cafe.name = params[:name]
+        @cafe.location = params[:location]
+        @cafe.wifi_avaliable = params [:wifi_avaliable]
+        @cafe.save
+        redirect '/cafes'
+    end 
+
+    delete '/cafes/:id' do 
+        @cafe = Cafe.find_by_id(params[:id])
+        @cafe.delete
+        redirect '/cafes'
+    end 
 
     
   end 
