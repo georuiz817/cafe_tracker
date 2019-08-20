cd4b387e54fc1703ddd59b28be48522fc276aa6b
George Ruiz <georuiz817@gmail.com>
2019-08-20 04:25:30 +0000

    edits

--- app/controllers/cafe_controller.rb
+++ app/controllers/cafe_controller.rb
@@ -9,36 +9,10 @@ class CafeController < ApplicationController
     end
 
     post '/cafes' do
-      @cafe = Cafe.create(params)
-      redirect "/"
+     @cafe = current_user.cafes.create(name: params["name"],location: params["location"],wifi_avaliable: params["wifi_avaliable"])
     end 
 
 
-    
-    get '/cafes/:id' do
-        @cafe = Cafe.find_by_id(params[:id])
-        erb :'cafes/show'
-    end 
-
-    get '/cafes/:id/edit' do 
-        @cafe = Cafe.find_by_id(params[:id])
-        erb :'cafes/edit'
-    end 
-
-    patch '/cafes/:id' do 
-        @cafe = Cafe.find_by_id(params[:id])
-        @cafe.name = params[:name]
-        @cafe.location = params[:location]
-        @cafe.wifi_avaliable = params [:wifi_avaliable]
-        @cafe.save
-        redirect '/cafes'
-    end 
-
-    delete '/cafes/:id' do 
-        @cafe = Cafe.find_by_id(params[:id])
-        @cafe.delete
-        redirect '/cafes'
-    end 
     
   end 
 
